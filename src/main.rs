592c3eadf3961e437d7f76fa5f3c0f6eabd51535
unknown <tsvetkovprelude@gmail.com>
2024-07-25 13:02:31 +0300

    day8 part1 in main: simple counting with Hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,43 @@
+use std::collections::HashMap;
+
 fn main() {
-    let input: Vec<String> = include_str!("../data/day2.txt").lines()
-                                                             .map(|x| x.to_string())
-                                                             .collect();
+    let input: Vec<String> = include_str!("../data/day8.txt").split("\r\n").map(|x| x.to_owned()).collect();
 
-    let mut valid: u32 = 0;
+    println!("{:?}", input);
+    
+    let mut visited_instructions: HashMap<usize, usize> = HashMap::new(); 
+    let mut acc: isize = 0;
+   
 
-    for item in input.clone() {
-        let mut parse: Vec<String> = item.split_whitespace().map(|s| s.to_string()).collect();
-        let range: Vec<u32> = parse[0].split('-').map(|n| n.parse().unwrap()).collect();
+    let mut id = 0;
 
-        let min = range[0];
-        let max = range[1];
+    'main: while id < input.len() {
+        let item = input[id].clone();
 
-        parse[1].truncate(1);
+        println!("{} at {}", item, id);
 
-        let ch: Vec<char> = parse[1].chars().collect();
-        let occ = parse[2].chars().filter(|c| *c == ch[0]).count() as u32;
-
-        if occ >= min && occ <= max {
-            valid += 1;
+        if let Some(_) = visited_instructions.get(&id) {
+            println!("SEEN a second time: instruction id: {}, instruction: {}", id, item);
+            println!("{}", acc);
+            break 'main;
         }
-    }
 
-    println!("part 1 {}", valid);
+        visited_instructions.entry(id).or_insert(1);
 
-
-    let mut valid: u32 = 0;
-
-    for item in input.clone() {
-        let mut parse: Vec<String> = item.split_whitespace().map(|s| s.to_string()).collect();
-        let valid_indices: Vec<u32> = parse[0].split('-').map(|n| n.parse().unwrap()).collect();
-
-        let one = valid_indices[0];
-        let two = valid_indices[1];
-
-        parse[1].truncate(1);
-
-        let ch: Vec<char> = parse[1].chars().collect();
-
-        let ans: bool = (parse[2].chars().nth((one as usize) - 1).unwrap() == ch[0]) ^ (parse[2].chars().nth((two as usize) - 1).unwrap() == ch[0]);
-        
-        if ans {
-            println!("Found");
-            valid += 1;
+        if item.starts_with("jmp") {
+            if let Ok(offset) = item[4..].trim().parse::<isize>() {
+                let new_index = id as isize + offset;
+                if new_index >= 0 && new_index < input.len() as isize {
+                    id = new_index as usize;
+                    continue;
+                }
+            }
+        } else if item.starts_with("acc") {
+            if let Ok(offset) = item[4..].trim().parse::<isize>() {
+                acc += offset; 
+            }
         }
+
+        id += 1;
     }
-
-    println!("part 2 {}", valid);
-
 }
