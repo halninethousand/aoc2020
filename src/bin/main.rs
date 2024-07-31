71ab0ec9a3b915b4890ecfb7d238b5df4d6cf111
unknown <tsvetkovprelude@gmail.com>
2024-07-31 15:45:03 +0300

    main has to be there as well, toml default-run

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut input_vec: Vec<Vec<char>> = include_str!("../data/day11.txt")
+    let mut input_vec: Vec<Vec<char>> = include_str!("../../data/day11.txt")
         .lines()
         .map(|line| line.chars().collect())
         .collect();
